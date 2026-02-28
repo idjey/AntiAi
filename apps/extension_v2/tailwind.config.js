4b05934d49cb34b2dae0678decf5a1af44688dce
idjey <djkanjaria@gmail.com>
2026-02-28 10:50:13 -0500

    feat: Add initial Tailwind CSS configuration file.

--- apps/extension_v2/tailwind.config.js
+++ apps/extension_v2/tailwind.config.js
@@ -3,5 +3,8 @@ module.exports = {
     mode: "jit",
     darkMode: "class",
     content: ["./**/*.{ts,tsx}", "!./node_modules/**", "!./build/**", "!./.plasmo/**"],
+    corePlugins: {
+        preflight: false,
+    },
     plugins: []
 }
