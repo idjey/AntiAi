d86439b65dc5d8cbd0cb1e5baf415a9bf57bf586
idjey <djkanjaria@gmail.com>
2026-04-16 12:10:53 -0400

    feat: implement viral anomaly detection cron service and initialize web and extension project structures

--- apps/api/src/modules/analytics/analytics.module.ts
+++ apps/api/src/modules/analytics/analytics.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { AnalyticsController } from './analytics.controller';
 import { AnalyticsService } from './analytics.service';
 import { PrismaModule } from '../../prisma/prisma.module';
-import { AnalyticsGateway } from './analytics.gateway';
 import { AnalyticsCronService } from './analytics.cron.service';
 
 @Module({
     imports: [PrismaModule],
     controllers: [AnalyticsController],
-    providers: [AnalyticsService, AnalyticsGateway, AnalyticsCronService],
+    providers: [AnalyticsService, AnalyticsCronService],
     exports: [AnalyticsService],
 })
 export class AnalyticsModule { }
